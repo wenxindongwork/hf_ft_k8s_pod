816c7dec213c67555d55abb4f9614d14b98143c2
wenxindongwork <wenxindong@google.com>
2024-08-13 23:55:40 +0000

    clean up

--- post_exp.sh
+++ post_exp.sh
@@ -1,12 +1,9 @@
-rm -rf profile/
-tmux capture-pane -pS - > ~/tmux-buffer.txt 
-cp ~/tmux-buffer.txt tmux-buffer.txt 
-cp /finetune_long_prof.py finetune_long_prof.py
-cp /usr/local/lib/python3.10/site-packages/transformers/trainer.py trainer.py
-cp /llama8b_sft_trainer.py llama8b_sft_trainer.py
-cp /gemma.py gemma.py
-cp /llama8b_trainer.py llama8b_trainer.py
-cp -r /tmp/profile/plugins/profile profile
-rm -rf trace/ && mkdir trace && cp /tmp/trace* trace
+#!/bin/bash
 
-# cp /tmp/save1.ir.0 save1.ir.0
+exp_name=$1
+log_dir=$2
+gs_bucket="gs://wenxindong/huggingface_tuning/$exp_name"
+
+tmux capture-pane -pS - > ~/tmux-buffer.txt 
+gsutil cp ~/tmux-buffer.txt "$gs_bucket/tmux-buffer.txt"
+gsutil  -m cp -r $log_dir "$gs_bucket/profile"
