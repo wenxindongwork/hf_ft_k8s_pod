c6ef732cc2ce4634a08e64da97685d3c569385ac
wenxindongwork <wenxindong@google.com>
2024-08-14 15:05:51 +0000

    update scripts

--- post_exp.sh
+++ post_exp.sh
@@ -2,8 +2,9 @@
 
 exp_name=$1
 log_dir=$2
-gs_bucket="gs://wenxindong/huggingface_tuning/$exp_name"
+gs_bucket="gs://wenxindong/huggingface_tuning/${exp_name}/"
 
-tmux capture-pane -pS - > ~/tmux-buffer.txt 
-gsutil cp ~/tmux-buffer.txt "${gs_bucket}/tmux-buffer.txt"
-gsutil -m cp -r $log_dir "${gs_bucket}/profile"
+cd $log_dir
+tmux capture-pane -pS - > tmux-buffer.txt
+gsutil -m cp -r * $gs_bucket
+cd -
--- run_exp.sh
+++ run_exp.sh
@@ -2,71 +2,71 @@
 
 exp_name="llama8b_baseline"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_without_lora"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-NO_LORA=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+NO_LORA=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_lora_kv_proj"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-LORA_KV_PROJ=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+LORA_KV_PROJ=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_mark_step_before_opt"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_no_fsdp"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-NO_FSDP=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+NO_FSDP=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_trainer"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_lora_kv_proj_mark_step_before_opt"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-LORA_KV_PROJ=True MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+LORA_KV_PROJ=True MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="llama8b_lora_kv_proj_mark_step_before_opt_no_fsdp"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-NO_FSDP=True LORA_KV_PROJ=True MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+NO_FSDP=True LORA_KV_PROJ=True MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
 
 clear
 exp_name="trainer_with_mark_step"
 log_dir="/tmp/${exp_name}/"
-rm -rf log_dir
-mkdir log_dir
-MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_trainer.py
+rm -rf $log_dir
+mkdir $log_dir
+MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR=$log_dir python /llama8b_trainer.py
 bash /post_exp.sh $exp_name $log_dir
