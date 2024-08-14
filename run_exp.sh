228661dcf8cc60d48ec750930440f9b85d0ddf14
wenxindongwork <wenxindong@google.com>
2024-08-14 04:53:56 +0000

    add exp

--- run_exp.sh
+++ run_exp.sh
@@ -62,3 +62,11 @@ rm -rf log_dir
 mkdir log_dir
 NO_FSDP=True LORA_KV_PROJ=True MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_sft_trainer.py
 bash /post_exp.sh $exp_name $log_dir
+
+clear
+exp_name="trainer_with_mark_step"
+log_dir="/tmp/${exp_name}/"
+rm -rf log_dir
+mkdir log_dir
+MARK_STEP_BEFORE_OPT=True PYTHONBREAKPOINT=0 PROFILE_EPOCH=0 PROFILE_STEP=5 PROFILE_DURATION_MS=50000 PROFILE_LOGDIR="/tmp/${exp_name}/" python /llama8b_trainer.py
+bash /post_exp.sh $exp_name $log_dir
