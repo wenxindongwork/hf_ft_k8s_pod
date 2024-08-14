78c1fb4ba8493d0340c7ba2ec9675b3a48e9a547
wenxindongwork <wenxindong@google.com>
2024-08-14 02:31:38 +0000

    mkdir

--- post_exp.sh
+++ post_exp.sh
@@ -5,5 +5,5 @@ log_dir=$2
 gs_bucket="gs://wenxindong/huggingface_tuning/$exp_name"
 
 tmux capture-pane -pS - > ~/tmux-buffer.txt 
-gsutil cp ~/tmux-buffer.txt "$gs_bucket/tmux-buffer.txt"
-gsutil  -m cp -r $log_dir "$gs_bucket/profile"
+gsutil cp ~/tmux-buffer.txt "${gs_bucket}/tmux-buffer.txt"
+gsutil -m cp -r $log_dir "${gs_bucket}/profile"
